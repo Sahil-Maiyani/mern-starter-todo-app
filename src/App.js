bba60fe041d1425f6c87512f5de376f1dc317e59
sahil <sahilmaiyani@gmail.com>
2019-03-17 21:03:32 +0530

    create empty comonents

--- src/App.js
+++ src/App.js
@@ -9,6 +9,9 @@ class App extends Component {
         <div className="container">
           <h2>MERN-Stack Todo App</h2>
         </div>
+        <Route path="/" exact component={TodosList} />
+        <Route path="/edit/:id" component={EditTodo} />
+        <Route path="/create" component={CreateTodo} />
       </Router>
     );
   }
