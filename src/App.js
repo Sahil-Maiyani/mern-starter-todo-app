73fda8cf4fa76201d52fc3482e5767eef63576d1
sahil <sahilmaiyani@gmail.com>
2019-03-17 21:32:51 +0530

    add basic layout for to-do

--- src/App.js
+++ src/App.js
@@ -2,16 +2,38 @@ import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {BrowserRouter as Router, Route, Link} from "react-router-dom";
 
+import CreateTodo from "./components/create-todo.component";
+import EditTodo from "./components/edit-todo.component";
+import TodosList from "./components/todos-list.component";
+
+import logo from './logo.svg'
+
 class App extends Component {
   render() {
     return (
       <Router>
         <div className="container">
-          <h2>MERN-Stack Todo App</h2>
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <a class="navbar-brand" href="index.js" target="_blank">
+              <img src={logo} width="50" height="50" alt="Mern-ToDo" />
+            </a>
+            <Link to="/" className="navbar-brand">My ToDo</Link>
+            <div className="collpase navbar-collapse">
+              <ul className="navbar-nav mr-auto">
+                <li className="navbar-item">
+                  <Link to="/" className="nav-link">Todos</Link>
+                </li>
+                <li className="navbar-item">
+                  <Link to="/create" className="nav-link">Create Todo</Link>
+                </li>
+              </ul>
+            </div>
+          </nav>
+          <br/>
+          <Route path="/" exact component={TodosList} />
+          <Route path="/edit/:id" component={EditTodo} />
+          <Route path="/create" component={CreateTodo} />
         </div>
-        <Route path="/" exact component={TodosList} />
-        <Route path="/edit/:id" component={EditTodo} />
-        <Route path="/create" component={CreateTodo} />
       </Router>
     );
   }
