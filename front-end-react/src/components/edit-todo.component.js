c35cb3abfaeed68d417cbbc9160bb439f936a5a3
sahil <sahilmaiyani@gmail.com>
2019-03-28 14:40:01 +0530

    put alert for todo update

--- front-end-react/src/components/edit-todo.component.js
+++ front-end-react/src/components/edit-todo.component.js
@@ -149,8 +149,6 @@ export default class EditTodo extends Component {
         };
         console.log(obj);
         axios.post('http://localhost:4000/todos/update/'+this.props.match.params.id, obj)
-            .then(res => console.log(res.data));
-
-        // this.props.history.push('/');
+            .then(res => alert(res.data));
     }
 }
