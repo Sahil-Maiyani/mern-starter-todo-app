eaffed51ca74af53186069db3e0c2ab5c4919119
sahil <sahilmaiyani@gmail.com>
2019-03-28 14:34:27 +0530

    remove duplication

--- backend-node-express/server.js
+++ backend-node-express/server.js
@@ -66,25 +66,6 @@ todoRoutes.route('/update/:id').post(function(req, res) {
     });
 });
 
-todoRoutes.route('/update/:id').post(function(req, res) {
-    Todo.findById(req.params.id, function(err, todo) {
-        if (!todo)
-            res.status(404).send("data is not found");
-        else
-            todo.todo_description = req.body.todo_description;
-            todo.todo_responsible = req.body.todo_responsible;
-            todo.todo_priority = req.body.todo_priority;
-            todo.todo_completed = req.body.todo_completed;
-
-            todo.save().then(todo => {
-                res.json('Todo updated!');
-            })
-            .catch(err => {
-                res.status(400).send("Update not possible");
-            });
-    });
-});
-
 app.listen(PORT, function() {
     console.log("Server is running on Port: " + PORT);
 });
